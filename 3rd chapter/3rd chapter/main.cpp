dc650d42e5d666da9a6e2291963ba1571f012b7b
YiPing <YiPing@YIPING-PC>
2020-01-26 10:48:11 +0800

    1

--- 3rd chapter/3rd chapter/main.cpp
+++ 3rd chapter/3rd chapter/main.cpp
@@ -27,7 +27,7 @@ int main()
 	return 0;
 }													*/
 
-//exceed.cpp -- exceeding some integer limits
+/*//2.exceed.cpp -- exceeding some integer limits
 #include <iostream>
 #define ZERO 0			// makes ZERO symbol for 0 value
 #include <climits>		// defines INT_MAX as largest int value
@@ -56,6 +56,87 @@ int main()
 
 	cin.get();
 	return 0;
-}
+}													*/
 
+/*//3.hexoct1.cpp -- shows hex and octal literals
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int chest = 42;		// decimal integer literal
+	int waist = 0x42;	// hexadecimal integer literal
+	int inseam = 042;	// octal integer literal
+
+	cout << "Monsieur cuts a striking figure!\n";
+	cout << "chest = " << chest << " (42 in decimal)\n";
+	cout << "waist = " << waist << " (0x42 in hex)\n"
+		 << "inseam = " << inseam << " (042 in octal)\n";
+	cin.get();
+	return 0;
+}													*/
+
+/*//4.hexoct2.cpp -- display values in hex and octal
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int chest = 42;		
+	int waist = 42;	
+	int inseam = 42;	
+
+	cout << "Monsieur cuts a striking figure!\n";
+	cout << "chest = " << chest << " (42 in decimal)\n";
+	cout << hex;	// manipulator for changing number base
+	cout << "waist = " << waist << " (0x42 in hex)\n";
+	cout << oct;	// manipulator for changing number base
+	cout << "inseam = " << inseam << " (042 in octal)\n";
+	cin.get();
+	return 0;
+}													*/
+
+/*//5.chartype.cpp
+#include <iostream>
+int main()
+{
+	using namespace std;
+	char ch;	// declare a char variable
+
+	cout << "Enter a character: " << endl;
+	cin >> ch;
+	cout << "Hola! ";
+	cout << "Thank you for the " << ch << " character." << endl;
+	cin.get();
+	cin.get();
+	return 0;
+}													*/
+
+//6. morechar.cpp -- the char type and int type contrasted
+#include <iostream>
+int main()
+{
+	using namespace std;
+	char ch = 'M';		// assign ASCII code for M to ch
+	int i = ch;			// store same code in an int
+	cout << "The ASCII code for " << ch << " is " << i << endl;
+
+	cout << "Add one to the character code:" << endl;
+	ch = ch + 1;		// change character code in ch
+	i = ch;			// save new character code in i
+	cout << "The ASCII code for " << ch << " is " << i << endl;
+
+	// using the cout.put() member function to display a charh
+	cout << "Displaying char ch using cout.put(ch): ";
+	cout.put(ch);
+	
+	// using cout.put() to display a char constant
+	cout.put('!');
+	cout << endl << "Done" << endl;
+
+	char alarm = '\a';
+	cout << alarm << "Don't do that again!\a\n";
+	cout << "Ben \"Buggsie\" Hacker\nwas here!\n";
+
+	cin.get();
+	return 0;
+}
 
